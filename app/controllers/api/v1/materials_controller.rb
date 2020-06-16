bde67a6027869d323c542913f9f0704bbc7e8298
Mel <melchrstmn@gmail.com>
2020-06-16 15:09:26 -0400

    added create action, strong params

--- app/controllers/api/v1/materials_controller.rb
+++ app/controllers/api/v1/materials_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::MaterialsController < ApplicationController
         materials = Material.all
         render json: materials
     end
+
+    def create
+        material = Material.new(material_params)
+        if material.save
+            render json: material, status: :accepted
+        else
+            render json: {errors: material.errors.full_messages}, status: :unprocessible_entity
+        end
+    end
+
+    private
+
+    def material_params
+        params.require(:material).permit(:title, :description, :url, :category_id)
+    end
+
 end
